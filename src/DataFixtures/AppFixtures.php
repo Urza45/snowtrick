a3afb38c529c3139544fa5f61d49ca01007b905b
Serge Pillay <urza45@live.fr>
2022-03-10 11:11:59 +0100

    Create fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Avatar;
+use App\Entity\Category;
+use App\Entity\Comment;
+use App\Entity\Media;
+use App\Entity\Trick;
+use App\Entity\TypeMedia;
+use App\Entity\TypeUser;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // Type User
+        $labelsTypeUser = ['Visiteur', 'Utilisateur enregistré', 'Administrateur'];
+
+        foreach ($labelsTypeUser as $labelTypeUser) {
+            $typeUser = new TypeUser();
+            $typeUser->setLabel($labelTypeUser);
+
+            $manager->persist($typeUser);
+        }
+
+        // Avatar
+        $avatar = new Avatar();
+        $avatar->setLegend('Femme')
+            ->setType('jpg')
+            ->setUrl('\media\avatars\womanProfils.jpg');
+
+        $manager->persist($avatar);
+
+        $avatar = new Avatar();
+        $avatar->setLegend('Homme')
+            ->setType('jpg')
+            ->setUrl('\media\avatars\manProfils.jpg');
+
+        $manager->persist($avatar);
+
+        // Admin User
+        $date = new \DateTime();
+        $date->setDate(2022, 2, 3);
+
+        $userAdmin = new User();
+        $userAdmin->setLastName('Doe')
+            ->setFirstName('John')
+            ->setPseudo('Admin')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setEmail('mon@email.fr')
+            ->setPhone('')
+            ->setCellPhone('')
+            ->setPassword('$2y$13$i/fr91Ni.lzPnuYIIXh5Leq1R./GBxQm14U.D0ePsOwhJvUT0Ze..')
+            ->setSalt('')
+            ->setStatusConnected(false)
+            ->setActivatedUser(true)
+            ->setValidationKey('-------------------------')
+            ->setTypeUser($typeUser)
+            ->setCreatedAt($date)
+            ->setAvatar($avatar);
+
+        $manager->persist($userAdmin);
+
+        // Category
+        $categoriesNames = ['Grabs', 'Rotations', 'Flips', 'Rotations désaxées', 'Slides', 'One foot', 'Old school'];
+
+        foreach ($categoriesNames as $categoryName) {
+            $category = new Category();
+            $category->setLabel($categoryName);
+
+            $manager->persist($category);
+
+            // Trick
+            $trick = new Trick();
+            $trick->setTitle('Un exemple de figure de ' . $category->getLabel())
+                ->setChapo('Une courte description de cette figure de  snowboard libre.')
+                ->setContent('Ici sera la description détaillée')
+                ->setCreatedAt($date)
+                ->setUpdatedAt($date)
+                ->setCategory($category)
+                ->setUser($userAdmin);
+
+            $manager->persist($trick);
+
+            // Add 4-6 comments
+            $now = new \DateTime('NOW');
+
+            for ($i = 1; $i <= mt_rand(4, 6); $i++) {
+                $comment = new Comment();
+                $comment->setContent('Voici le commentaire n°' . $i)
+                    ->setCreatedAt($now)
+                    ->setUpdatedAt($now)
+                    ->setDisabled(false)
+                    ->setNew(false)
+                    ->setUser($userAdmin)
+                    ->setTrick($trick);
+
+                $manager->persist($comment);
+            }
+        }
+
+        // Type media
+        $typeMedia = new TypeMedia();
+        $typeMedia->setGroupMedia('Vidéo')
+            ->setTypeMedia('mp4');
+
+        $manager->persist($typeMedia);
+
+        $typeMedia = new TypeMedia();
+        $typeMedia->setGroupMedia('Image')
+            ->setTypeMedia('jpg');
+
+        $manager->persist($typeMedia);
+
+        $manager->flush();
+    }
+}
