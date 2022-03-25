4b4498290fbf26b531c5db3bbe63f04ee75b6c04
Serge Pillay <urza45@live.fr>
2022-03-25 18:06:12 +0100

    Added modification of a trick - Text fields

--- assets/app.js
+++ assets/app.js
@@ -13,3 +13,51 @@ import { Tooltip, Toast, Popover } from 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
+
+$(function () {
+    var page = 1
+
+    $("#showMoreTricks").on("click", function () {
+
+        var index = $("#showMoreTricks").data("index");
+
+        var nbTrickByPage = $(this).data("tricksbypage");
+        var nbtrick = $(this).data("nbtrick");
+        var newNbTrick = page * nbTrickByPage;
+        var urlDest = "/showmoretrick/" + newNbTrick;
+        // AJAX request
+        $.ajax({
+            url: urlDest,
+            type: "post",
+            data: {},
+            success: function (response) {
+                $("#listTrick").append(response);
+                if ((newNbTrick + nbTrickByPage) < nbtrick) {
+                    page++;
+                } else {
+                    $('#trickplus').hide();
+                }
+            },
+            error: function () {
+                alert('Error');
+            }
+        });
+    });
+
+    let items = document.querySelectorAll('.carousel .carousel-item')
+
+    items.forEach((el) => {
+        const minPerSlide = 4
+        let next = el.nextElementSibling
+        for (var i = 1; i < minPerSlide; i++) {
+            if (!next) {
+                // wrap carousel by using first child
+                next = items[0]
+            }
+            let cloneChild = next.cloneNode(true)
+            el.appendChild(cloneChild.children[0])
+            next = next.nextElementSibling
+        }
+    })
+})
+
--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,22 +2,80 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\TrickType;
+use App\Repository\TrickRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrickController extends AbstractController
 {
+    private const NUMBER_TRICK_BY_ROW = 3;
+    private const NUMBER_TRICK_BY_PAGE = 3;
+
     /**
      * @Route("/", name="trick_home")
      */
-    public function index(): Response
+    public function index(TrickRepository $repoTrick): Response
     {
-
-
-
         return $this->render('trick/index.html.twig', [
-            'controller_name' => 'TrickController',
+            'tricksCount' => $repoTrick->count([]),
+            'numberTrickByRow' => self::NUMBER_TRICK_BY_ROW,
+            'numberTrickByPage' => self::NUMBER_TRICK_BY_PAGE,
+            'tricks' => $repoTrick->findBy([], ['id' => 'ASC'], self::NUMBER_TRICK_BY_PAGE, 0)
+        ]);
+    }
+
+    /**
+     * @Route("/showmoretrick/{index}", name="show_more_trick", methods={"GET", "POST"},requirements={"index"="\d+"})
+     */
+    public function showMoreTrick(Request $request, TrickRepository $repoTrick)
+    {
+        return $this->render('trick/show_more_trick.html.twig', [
+            'tricksCount' => $repoTrick->count([]),
+            'numberTrickByRow' => self::NUMBER_TRICK_BY_ROW,
+            'numberTrickByPage' => self::NUMBER_TRICK_BY_PAGE,
+            'index' => $request->get('index'),
+            'tricks' => $repoTrick->findBy([], ['id' => 'ASC'], self::NUMBER_TRICK_BY_PAGE, $request->get('index'))
+        ]);
+    }
+
+    /**
+     * @Route("/trick/{id}", name="show_trick")
+     */
+    public function showTrick(TrickRepository $repoTrick, Request $request)
+    {
+        $trick = $repoTrick->findOneBy(['id' => $request->get('id')]);
+
+        return $this->render('trick/show_trick.html.twig', [
+            'trick' => $trick
+        ]);
+    }
+
+    /**
+     * @Route("/modify_trick/{id}", name="modify_trick")
+     */
+    public function modifyTrick(TrickRepository $repoTrick, Request $request, ManagerRegistry $doctrine)
+    {
+        $trick = $repoTrick->findOneBy(['id' => $request->get('id')]);
+
+        $form = $this->createForm(TrickType::class, $trick);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $doctrine->getManager();
+
+            $manager->persist($trick);
+            $manager->flush();
+
+            $this->addFlash('success', 'Vos modification ont bien été enregistrées.');
+        };
+
+        return $this->render('trick/modify_trick.html.twig', [
+            'form' => $form->createView(),
+            'trick' => $trick
         ]);
     }
 }
