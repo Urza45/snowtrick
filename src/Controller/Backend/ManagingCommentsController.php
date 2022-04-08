345ee1c006ffba1c721ddc23730e54a92c0a0630
Serge Pillay <urza45@live.fr>
2022-04-08 18:33:59 +0200

    Add administrator managùent of comments

--- src/Controller/Backend/ManagingCommentsController.php
+++ src/Controller/Backend/ManagingCommentsController.php
@@ -2,19 +2,57 @@
 
 namespace App\Controller\Backend;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ManagingCommentsController extends AbstractController
 {
     /**
      * @Route("/admin/managing/comments", name="app_managing_comments")
      */
-    public function index(): Response
+    public function index(CommentRepository $repoComment): Response
     {
+        $comments = $repoComment->findAll();
+
         return $this->render('managing_comments/index.html.twig', [
-            'controller_name' => 'ManagingCommentsController',
+            'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/admin/managing/comment/{id}", name="app_managing_comment")
+     */
+    public function modifyComment(Request $request, CommentRepository $repoComment, ManagerRegistry $doctrine)
+    {
+        $comment = $repoComment->findOneBy(['id' => $request->get('id')]);
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        return $this->render('managing_comments/show_comment.html.twig', [
+            'form' => $form->createView(),
+            'requete' => $request
+        ]);
+    }
+
+    /**
+     * @Route("/admin/managing/comment_ban/{id}", name="app_managing_ban_comment")
+     */
+    public function banComment(Request $request, CommentRepository $repoComment, ManagerRegistry $doctrine)
+    {
+        return new Response('Hello');
+    }
+
+    /**
+     * @Route("/admin/managing/comment_delete/{id}", name="app_managing_delete_comment")
+     */
+    public function deleteComment(Request $request, CommentRepository $repoComment, ManagerRegistry $doctrine)
+    {
+        return new Response('Hello');
     }
 }
--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,20 +2,25 @@
 
 namespace App\Controller;
 
-use App\Service\Captcha;
+use App\Entity\Comment;
 use App\Form\TrickType;
+use App\Service\Captcha;
+use App\Form\CommentType;
+use App\Repository\CommentRepository;
+use App\Repository\UserRepository;
 use App\Repository\TrickRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrickController extends AbstractController
 {
     private const NUMBER_TRICK_BY_ROW = 3;
     private const NUMBER_TRICK_BY_PAGE = 3;
+    private const NUMBER_COMMENT_BY_PAGE = 5;
 
     /**
      * @Route("/", name="trick_home")
@@ -45,23 +50,76 @@ class TrickController extends AbstractController
     }
 
     /**
-     * @Route("/trick/{id}", name="show_trick")
+     * @Route("/showmorecomment/{index}", name="show_more_comment", methods={"GET", "POST"},requirements={"index"="\d+"} )
      */
-    public function showTrick(TrickRepository $repoTrick, Request $request)
+    public function showMoreComment(Request $request, TrickRepository $repoTrick, CommentRepository $repoComment)
     {
-        $trick = $repoTrick->findOneBy(['id' => $request->get('id')]);
+        dump($request);
+        $trick = $repoTrick->findOneBy(['id' => $request->get('trickId')]);
 
-        return $this->render('trick/show_trick.html.twig', [
-            'trick' => $trick
+        return $this->render('comment/index.html.twig', [
+            'trick' => $trick,
+            'listComment' => $repoComment->findBy(['trick' => $trick], ['id' => 'DESC'], self::NUMBER_COMMENT_BY_PAGE, $request->get('index')),
+            'commentsCount' => $repoComment->count(['trick' => $trick]),
+            'index' => $request->get('index'),
+            'numberCommentByPage' => self::NUMBER_COMMENT_BY_PAGE,
         ]);
     }
 
     /**
-     * @Route("/modify_trick/{id}", name="modify_trick")
+     * @Route("/trick/{slug}", name="show_trick")
+     */
+    public function showTrick(
+        TrickRepository $repoTrick,
+        Request $request,
+        Session $session,
+        UserRepository $repoUser,
+        CommentRepository $repoComment,
+        ManagerRegistry $doctrine
+    ) {
+        $trick = $repoTrick->findOneBy(['slug' => $request->get('slug')]);
+        $comment = new Comment();
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $doctrine->getManager();
+            $user = $repoUser->findOneByPseudo($this->getUser()->getUserIdentifier());
+
+            // Captcha verification
+            if (!($form->get('captcha')->getData() == $session->get('captcha'))) {
+                $this->addFlash('comment', 'Le captcha saisi n\'est pas correct.');
+                return $this->render('trick/show_trick.html.twig', [
+                    'trick' => $trick,
+                    'formComment' => $form->createView()
+                ]);
+            }
+            $comment->setDisabled(false);
+            $comment->setNew(true);
+            $comment->setUser($user);
+            $comment->setTrick($trick);
+
+            $manager->persist($comment);
+            $manager->flush();
+            $this->addFlash('success', 'Votre commentaire a bien été enregistré.');
+        }
+
+        return $this->render('trick/show_trick.html.twig', [
+            'trick' => $trick,
+            'formComment' => $form->createView(),
+            'listComment' => $repoComment->findBy(['trick' => $trick], ['id' => 'DESC'], self::NUMBER_COMMENT_BY_PAGE, 0),
+            'commentsCount' => $repoComment->count(['trick' => $trick]),
+            'numberCommentByPage' => self::NUMBER_COMMENT_BY_PAGE,
+        ]);
+    }
+
+    /**
+     * @Route("/modify_trick/{slug}", name="modify_trick")
      */
     public function modifyTrick(TrickRepository $repoTrick, Request $request, ManagerRegistry $doctrine)
     {
-        $trick = $repoTrick->findOneBy(['id' => $request->get('id')]);
+        $trick = $repoTrick->findOneBy(['slug' => $request->get('slug')]);
 
         $form = $this->createForm(TrickType::class, $trick);
         $form->handleRequest($request);
@@ -72,7 +130,7 @@ class TrickController extends AbstractController
             $manager->persist($trick);
             $manager->flush();
 
-            $this->addFlash('success', 'Vos modification ont bien été enregistrées.');
+            $this->addFlash('success', 'Vos modifications ont bien été enregistrées.');
         };
 
         return $this->render('trick/modify_trick.html.twig', [
